cd231a0988238ca0d55d862f8e8117d5d64e1ae8
Andrew <rogaicopyta@example.com>
2023-06-09 12:09:25 +0300

    x-secret-key

--- src/api/api.ts
+++ src/api/api.ts
@@ -16,7 +16,7 @@ import { AuthDataType } from '../types/authDataType';
 
 const headers = {
   //'Access-Control-Allow-Headers': '*',
-  // 'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
+  'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
   'X-Api-App-Id':
     'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
 };
