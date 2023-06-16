e42fb53bea235b80f775c52bd7570a4704418bcc
Andrew9614 <54529269+Andrew9614@users.noreply.github.com>
2023-06-16 17:01:28 +0300

    cors suka

--- src/globalVars/routes.ts
+++ src/globalVars/routes.ts
@@ -2,7 +2,7 @@ export const FAVORITES_PAGE_URL = '/favorites';
 
 //API URLs
 export const BASE_URL_API =
-  'https://api.superjob.ru/2.0/';
+  'https://startup-summer-2023-proxy.onrender.com/2.0/';
 export const VACANCIES_URL_API = 'vacancies/';
 export const CATALOGUES_URL_API = 'catalogues/';
 export const AUTH_ON_PASSWORD_URL = 'oauth2/password/';
