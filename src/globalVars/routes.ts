eea29c338b2bc87698ea1516cf78300e74ab63ec
Andrew <rogaicopyta@example.com>
2023-06-23 13:26:10 +0300

    cors with proxy

--- src/globalVars/routes.ts
+++ src/globalVars/routes.ts
@@ -1,8 +1,7 @@
 export const FAVORITES_PAGE_URL = '/favorites';
 
 //API URLs
-export const BASE_URL_API =
-  'https://startup-summer-2023-proxy.onrender.com/2.0/';
+export const BASE_URL_API = 'http://localhost:8010/proxy';
 export const VACANCIES_URL_API = 'vacancies/';
 export const CATALOGUES_URL_API = 'catalogues/';
 export const AUTH_ON_PASSWORD_URL = 'oauth2/password/';
