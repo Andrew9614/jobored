14e72d8d9a590fd7b75a68fdef142127258d8d5c
Andrew <rogaicopyta@example.com>
2023-06-28 12:27:01 +0300

    proxy fix

--- src/globalVars/routes.ts
+++ src/globalVars/routes.ts
@@ -1,7 +1,7 @@
 export const FAVORITES_PAGE_URL = '/favorites';
 
 //API URLs
-export const BASE_URL_API = 'http://localhost:8010/proxy';
+export const BASE_URL_API = 'https://proxy-jobored.onrender.com/proxy';
 export const VACANCIES_URL_API = 'vacancies/';
 export const CATALOGUES_URL_API = 'catalogues/';
 export const AUTH_ON_PASSWORD_URL = 'oauth2/password/';
